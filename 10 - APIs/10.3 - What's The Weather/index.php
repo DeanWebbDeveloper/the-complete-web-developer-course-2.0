08d1e9918e277a203135aa5246ca37757f66d170
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-04-21 22:06:33 +0100

    Renaming to follow convention
    Renaming and cleaning up files to follow convention of other courses. Including missing folders and files
    
    [Course chapter no].[Lecture in course file] - [Name of lecture]

--- 10 - APIs/10.3 - What's The Weather/index.php
+++ 10 - APIs/10.3 - What's The Weather/index.php
@@ -1,28 +1,28 @@
 <?php
-    
+
     $weather = "";
     $error = "";
-    
+
     if ($_GET['city']) {
-        
+
      $urlContents = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET['city']).",uk&appid=4b6cbadba309b7554491c5dc66401886");
-        
+
         $weatherArray = json_decode($urlContents, true);
-        
+
         if ($weatherArray['cod'] == 200) {
-        
+
             $weather = "The weather in ".$_GET['city']." is currently '".$weatherArray['weather'][0]['description']."'. ";
 
             $tempInCelcius = intval($weatherArray['main']['temp'] - 273);
 
             $weather .= " The temperature is ".$tempInCelcius."&deg;C and the wind speed is ".$weatherArray['wind']['speed']."m/s.";
-            
+
         } else {
-            
+
             $error = "Could not find city - please try again.";
-            
+
         }
-        
+
     }
 
 
@@ -40,79 +40,79 @@
       <title>Weather Scraper</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-      
+
       <style type="text/css">
-      
-      html { 
-          background: url(background.jpeg) no-repeat center center fixed; 
+
+      html {
+          background: url(images/background.jpeg) no-repeat center center fixed;
           -webkit-background-size: cover;
           -moz-background-size: cover;
           -o-background-size: cover;
           background-size: cover;
           }
-        
+
           body {
-              
+
               background: none;
-              
+
           }
-          
+
           .container {
-              
+
               text-align: center;
               margin-top: 100px;
               width: 450px;
-              
+
           }
-          
+
           input {
-              
+
               margin: 20px 0;
-              
+
           }
-          
+
           #weather {
-              
+
               margin-top:15px;
-              
+
           }
-         
+
       </style>
-      
+
   </head>
   <body>
-    
+
       <div class="container">
-      
+
           <h1>What's The Weather?</h1>
-          
-          
-          
+
+
+
           <form>
   <fieldset class="form-group">
     <label for="city">Enter the name of a city.</label>
     <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo" value = "<?php echo $_GET['city']; ?>">
   </fieldset>
-  
+
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-      
-          <div id="weather"><?php 
-              
+
+          <div id="weather"><?php
+
               if ($weather) {
-                  
+
                   echo '<div class="alert alert-success" role="alert">
   '.$weather.'
 </div>';
-                  
+
               } else if ($error) {
-                  
+
                   echo '<div class="alert alert-danger" role="alert">
   '.$error.'
 </div>';
-                  
+
               }
-              
+
               ?></div>
       </div>
 
