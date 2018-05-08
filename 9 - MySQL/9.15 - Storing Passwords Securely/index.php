842d2a6bf8d5038d4270c529dc84d226d0377793
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-05-08 21:37:24 +0100

    Revising MySQL - Project
    Going over MySQL project, to continue next time

--- 9 - MySQL/9.15 - Storing Passwords Securely/index.php
+++ 9 - MySQL/9.15 - Storing Passwords Securely/index.php
@@ -4,20 +4,16 @@
 
     $row['id'] = 73;
 
-    //echo md5(md5($row['id'])."password");
+    echo md5(md5($row['id'])."password");
 
     //md5 is not secure, it is much better to use bcrypt. PHP >= 5.5 has this built-in, so look into using this instead
 
-    echo password_hash('password', PASSWORD_DEFAULT);
+    $hash = password_hash("rasmuslerdorf", PASSWORD_DEFAULT);
 
-    $hash = "$2y$10$ND.K4MKescwMgu2GbkQbgO5mDCSV5aAfmMHJ9Ns6AKQvuvrIVNxXO";
-
-    /*if (password_verify('password', $hash)) {
+    if (password_verify('rasmuslerdorf', $hash)) {
         echo 'Password is valid!';
     } else {
         echo 'Invalid password.';
-    }*/
-
-    //Look into bcrypt before moving on//
+    }
 
 ?>
