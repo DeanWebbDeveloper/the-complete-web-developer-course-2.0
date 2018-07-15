4e4718ad6c56a0bb1ca7eefe23c5c3fcc2881454
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-07-15 12:38:41 +0100

    Chapter 11 Project - Email Client App
    Completed Project

--- 11 - Mobile Apps/11.7 - Challenge - Email Client App/appjs/mailer.php
+++ 11 - Mobile Apps/11.7 - Challenge - Email Client App/appjs/mailer.php
@@ -2,6 +2,22 @@
 
   session_start();
 
-  echo ("hello")
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //set variables to post data
+    $sender     = "From: " . $_POST["sender"];
+    $recipient  = $_POST["recipient"];
+    $subject    = $_POST["subject"];
+    $message    = $_POST["message"];
+
+    if(mail($recipient, $subject, $message, $sender)) {
+      if (http_response_code(200)) {
+        return("Email successfully sent!");
+      } else {
+        return("Error - Email could not be sent. Please try again later!");
+      };
+    };
+
+  };
 
 ?>
